0045c492cd6bd0d4fffcb723d93d69209b4c3ad3
VSS <sandybreak3405@gmail.com>
2024-09-27 19:58:39 +0300

    UPDATE DIR admin

--- bot/admin/assistant.py
+++ bot/admin/assistant.py
@@ -1,70 +1,17 @@
 # -*- coding: UTF-8 -*-
 
-from aiogram.types import FSInputFile
 from typing import Optional, Tuple
-from aiogram import Bot
-
-import logging
-import json
-import os
-
-
-from dotenv import set_key
 
 
 class AdminOperations:
     def __init__(self) -> None:
         self.auth_client = None
-    
-    
-    async def get_manager_password(self) -> str:
-        """
-        Получение пароля админа бота
-        """
-        return os.environ.get('MANAGER_PASSWORD_HASH')
-    
-    
-    def get_attempts_enter_wrong_password(self) -> str:
-        """
-        Получение количества оставшихся попыток ввода пароля
-        """
-        return os.environ.get('MAX_ATTEMPTS')
-    
-    
-    async def update_attempts_enter_wrong_password(self, remaining_attempts: dict) -> str:
-        """
-        Обновление количества оставшихся попыток ввода пароля
-        """
-        set_key('docker-compose.env', 'MAX_ATTEMPTS', str(remaining_attempts))
-        return os.environ.get('MAX_ATTEMPTS')
-    
-    
-    async def update_list_admin_users(self, list_users: dict) -> str:
-        """
-        Обновление количества оставшихся попыток ввода пароля
-        """
-        try:
-            list_users = json.dumps(list_users)
-            set_key('docker-compose.env', 'ADMIN_USERS', str(list_users))
-        except Exception as e:
-            logging.error(f"Error during update_list_admin_users: {e}")
-    
-    
-    async def update_list_banned_users(self, list_users: dict) -> str:
-        """
-        Обновление количества оставшихся попыток ввода пароля
-        """
-        try:
-            list_users = json.dumps(list_users)
-            set_key('docker-compose.env', 'BANNED_USERS', str(list_users))
-        except Exception as e:
-            logging.error(f"Error during update_list_banned_users: {e}")
-                
-
-    async def parse_callback_data(self, data: str) -> Tuple[str, Optional[str], Optional[str]]:
-        """
+            
+    @staticmethod
+    async def parse_callback_data(data: str) -> Tuple[str, Optional[str], Optional[str]]:
+        '''
         Получение данных из строки callback.data
-        """
+        '''
         if ',' in data:
             parts = data.split(',')
             action = parts[0]
@@ -73,16 +20,3 @@ class AdminOperations:
             return action, user_id, user_tg_addr
         else:
             return data, None, None
-        
-        
-    async def save_photo(self, user_id: int, downloaded_photo) -> str:
-        """
-        Сохранение локально фото для рассылки 
-        """
-        try:
-            path_table = f'./downloads/{user_id}.jpg'
-            with open(path_table, 'wb') as f:
-                f.write(downloaded_photo.read())
-            return path_table
-        except Exception as e:
-            logging.error(e)
