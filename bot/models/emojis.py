a81322c322944121e692ac1231f8dd838ac0ae2c
VSS <sandybreak3405@gmail.com>
2024-09-27 11:38:19 +0300

    ADD models/emojis.py

---
+++ bot/models/emojis.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+class Emojis:
+    def __init__(self):
+        pass
+    
+    
+    TIME = '⏳'
+    
+    SUCCESS = '✅'
+    
+    FAIL = '❌'
+    
+    PRXT33 = '🟣'
+    
+    POINTER = '👇'
+    
+    ALLERT = '⚠️'
+    
+    HELLO = '👋'
+
+    
